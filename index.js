27afae5061acbe84fea2ef8dbf6c3b42280f772a
AnaVeisa <131899856+AnaVeisa@users.noreply.github.com>
2024-03-06 09:09:31 +0200

    lectia 13

---
+++ index.js
@@ -0,0 +1,133 @@
+//ex.2
+//let carModel = 'Volvo'
+//let DIFFICULT_TO_SPELL = 'Hyundai'
+//let Text = 'Hello'
+
+//ex.3
+// let str1 = 'Cine stie'
+// let str2 = 'carte,'
+// let str3 = 'are patru ochi'
+
+// console.log(`Un proverb vechi spune ${str1} ${str2} ${str3}`)
+
+//ex.4
+//m. 1 
+//let num = 10
+//console.log(num, typeof num)
+//console.log(typeof String (num))
+
+//m. 2
+//const num = 15
+//console.log(num, typeof num)
+//let convertetNum = String(num)
+//console.log(convertetNum, typeof convertetNum)
+
+//m.1
+//let num = 20
+//console.log(num, typeof num)
+//console.log(typeof Boolean (num))
+
+//m.2
+//let num = 25
+//console.log(num, typeof num)
+//let convertNum = Boolean(num)
+//console.log(convertNum, typeof convertNum)
+
+//ex.5
+//m.1
+//let str = 'Buna'
+//console.log(str, typeof str)
+//console.log(typeof Number (str))
+
+//let str = 'Hello'
+//console.log(str, typeof str)
+//console.log(typeof Boolean (str))
+
+//m.2
+//let str = 'Salut'
+//console.log(str, typeof str)
+//let convertStr = Number(str)
+//console.log(convertStr, typeof convertStr)
+//console.log(+convertStr)
+
+//let str = 'Ciao'
+//console.log(str, typeof str)
+//let convertStr = Boolean(str)
+//console.log(convertStr, typeof convertStr)
+
+//ex.6
+//let cunostinte = 0
+//if (cunostinte){
+  //  console.log('Daca nu ai cunostinte')
+//}
+//else {
+  //  console.log('nu ai nimic')
+//}
+
+// let cap = ''
+// if (cap){
+//     console.log('nu-i cap')
+// }
+// else {
+//     console.log('vai de picioare')
+// }
+
+// let inaltime
+// if (inaltime){
+//     console.log('Undefined')
+// }
+// else{
+//     console.log('nedefinit')
+// }
+
+// let sent = Boolean(10<8)
+// if (sent){
+//     console.log('e adevarat')
+// }
+// else{
+//     console.log('e fals')
+// }
+
+// let val = 5>2 || 4>10
+// if (val){
+//     console.log('prima')
+// }
+// else{
+//     console.log('a doua')
+// }
+
+// let num1 = 5
+// let num2 = 10
+// if (num1+num2 && 15) {
+//     console.log('and')
+// }
+// else{
+//     console.log('notand')
+// }
+
+//ex.7
+// let score = 80
+// let scoreRating =  score > 70 ? "Excellent" : "Do better"
+// console.log(scoreRating)
+
+// let temp = 10
+// let tempNota = temp > 15 ? 'Cald' : 'Racoare'
+// console.log(tempNota)
+
+//ex.8
+let str = 'lasagna'
+switch (str){
+    case 'omleta':
+    console.log('aleg omleta');
+    break
+    case 'paste':
+    console.log('aleg paste');
+    break
+    case 'lasagna':
+    console.log('aleg lasagna');
+    break
+    case 'desert':
+    console.log('desert va rog');
+    default:
+        console.log('nu doresc nimic')
+}
